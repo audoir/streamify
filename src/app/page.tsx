24c348bb76d492a8b2960e884af8ac59d013b31e
audoir <wayne@audoir.com>
2024-08-27 14:13:15 -0700

    api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+//-----------------------------------------------------------------------------
+// Author: Wayne Cheng
+// Description: Dashboard
+//-----------------------------------------------------------------------------
+
 import { Button } from "@mui/material";
 import CenterBox from "./components/CenterBox";
+import { DashboardData } from "@/lib/shared/model";
+import axios from "axios";
 
 export default function Home() {
   const onClick = async () => {
-    console.log(`Click clack`);
+    const rsp: DashboardData | undefined = await getDashboardData();
+    console.log(rsp);
   };
 
   return (
@@ -17,4 +25,16 @@ export default function Home() {
       </>
     </CenterBox>
   );
+}
+
+const getDashboardData = async (): Promise<DashboardData | undefined> => {
+  try {
+    const rsp = await axios.post("api/get-dashboard-data",
+      { authToken: "abc123" },
+    );
+    console.log(rsp.data);
+    return rsp.data as DashboardData;
+  } catch (e: any) {
+    console.log(`Cannot get dashboard data: ${e}`);
+  }
 }
